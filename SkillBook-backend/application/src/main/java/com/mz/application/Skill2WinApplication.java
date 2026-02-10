266c5f855df3d01b69b22f63c229135d30e93160
Margiorno <m.zawiski03@gmail.com>
2026-02-10 20:25:05 +0100

    empty projekt backbone

---
+++ SkillBook-backend/application/src/main/java/com/mz/application/Skill2WinApplication.java
@@ -0,0 +1,12 @@
+package com.mz.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.mz")
+public class Skill2WinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skill2WinApplication.class, args);
+    }
+}
