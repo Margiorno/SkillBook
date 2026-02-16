58f6c7f1526368ed8ea129848325b275caddbd4d
Margiorno <m.zawiski03@gmail.com>
2026-02-16 12:49:29 +0100

    empty identity module

--- SkillBook-backend/application/src/main/java/com/mz/application/SkillBookApplication.java
+++ SkillBook-backend/application/src/main/java/com/mz/application/SkillBookApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.mz")
-public class Skill2WinApplication {
+public class SkillBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Skill2WinApplication.class, args);
+        SpringApplication.run(SkillBookApplication.class, args);
     }
 }
